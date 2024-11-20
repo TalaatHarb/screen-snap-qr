78cd7ff5d9742b2a94c8b9a91171be7f4586a269
mharb <mohamed.harb@cegedim.com>
2024-11-20 21:23:19 +0200

    Basic implementation for snap shot

--- screen-snap-qr/src/main/java/net/talaatharb/screensnapqr/service/ScreenSnapServiceImpl.java
+++ screen-snap-qr/src/main/java/net/talaatharb/screensnapqr/service/ScreenSnapServiceImpl.java
@@ -1,8 +1,28 @@
 package net.talaatharb.screensnapqr.service;
 
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Robot;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ScreenSnapServiceImpl implements ScreenSnapService {
+
+	public BufferedImage takeSnapshot() throws Exception {
+		Rectangle2D allScreenBounds = new Rectangle2D.Double();
+		GraphicsEnvironment localGE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		for (GraphicsDevice gd : localGE.getScreenDevices()) {
+		  for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
+		    Rectangle2D.union(allScreenBounds, graphicsConfiguration.getBounds(), allScreenBounds);
+		  }
+		}
+
+		Robot robot = new Robot();
+		return robot.createScreenCapture(allScreenBounds.getBounds());
+	}
 
 }
